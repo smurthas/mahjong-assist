179f0e53eed1852430f3cd83ea0c18ca5b45bec2
Simon Murtha-Smith <simon@murtha-smith.com>
2013-12-26 18:20:08 -0500

    W() fn for calculating number of winning combos

--- test/lib.test.js
+++ test/lib.test.js
@@ -6,6 +6,7 @@ var TileSet = require('../tileSet');
 
 function nCr(n, r) {
   if (r < 1) return 1;
+  if (r > n) return 0;
   var val = 1;
   for (var i = n; i > r && i > n-r; i--) val *= i;
   var min = Math.min(r, n-r);
@@ -23,15 +24,30 @@ function P(tiles, needed, draw, redundant) {
   return nCr(tiles - needed, draw - needed)/nCr(tiles, draw);
 }
 
-function W(tiles, needed, draw, duplicates) {
-  var p = nCr(tiles - needed, draw - needed);
-  console.error('p', p);
-  console.error('duplicates', duplicates);
-  if(tiles <= draw || duplicates === 0) return p;
-  return p + W(tiles-1, needed, draw, duplicates-1);
+function W(tiles, needed, draw, duplicates, duplicateNeeded) {
+  var extra = draw - needed;
+  var p = 0;
+  var junk = tiles - needed - duplicates + duplicateNeeded;
+
+  var iMax = duplicates? Math.min(extra, duplicates-1): extra;
+  //console.error('junk: %d, iMax: %d, extra: %d', junk, iMax, extra);
+  for (var i = 0; i <= iMax; i++) {
+    var p1 = nCr(junk, extra - i);
+    var p2 = nCr(duplicates, duplicateNeeded + i);
+    p += p1 * p2;
+  }
+  return p;
 }
 
 describe('lib', function() {
+  describe('#nCr', function() {
+    it('should handle 0', function() {
+      assert.equal(nCr(5, 0), 1);
+    });
+    it('should handle  2 C 3', function() {
+      assert.equal(nCr(2, 3), 0);
+    });
+  });
   describe('#tilePerms', function() {
     it('should return the probability of getting mahjong', function() {
       var tiles = parseRack('F 0 1B 1D');
@@ -89,6 +105,14 @@ describe('lib', function() {
       assert.equal(lib.tilePerms(tiles, needed, 4), K(4-needed.length)/R(4));
       assert.equal(lib.tilePerms(tiles, needed, 5), K(5-needed.length)/R(5));
       assert.equal(lib.tilePerms(tiles, needed, 6), 1);
+
+      var duplicates = 0, duplicateNeeded = 0;
+      for (var i = needed.length; i <= tiles.length; i++) {
+        var num = W(tiles.length, needed.length, i, duplicates, duplicateNeeded);
+        var den = R(i);
+        //console.error('%d/%d', num, den);
+        assert.equal(lib.tilePerms(tiles, needed, i), num/den);
+      }
     });
 
     it('should return the probability of getting mahjong', function() {
@@ -96,24 +120,17 @@ describe('lib', function() {
       var needed = parseRack('F 0');
       var R = nCr.bind(this, tiles.length);
       var K = nCr.bind(this, tiles.length - needed.length);
-      var duplicates = 1;
+      var duplicates = 2;
       var L = nCr.bind(this, tiles.length - needed.length - duplicates);
       assert.equal(lib.tilePerms(tiles, needed, 0), 0);
       assert.equal(lib.tilePerms(tiles, needed, 1), 0);
       assert.equal(lib.tilePerms(tiles, needed, 2), 2*K(2-needed.length)/R(2));
+
       for (var i = needed.length; i <= tiles.length; i++) {
-        console.error('i', i);
-        var num = W(tiles.length, needed.length, i, duplicates);
+        var num = W(tiles.length, needed.length, i, duplicates, 1);
         var den = R(i);
-        console.error('num', num);
-        console.error('den', den);
         assert.equal(lib.tilePerms(tiles, needed, i), num/den);
       }
-      //console.error('L(3)', L(3 - needed.length));
-      //var num = (1+ duplicates)*L(3 - needed.length) + nCr(duplicates, needed.length)
-      //assert.equal(lib.tilePerms(tiles, needed, 3),  /R(3));
-      assert.equal(lib.tilePerms(tiles, needed, 3), (2*K(3-needed.length) - 1)/R(3));
-      assert.equal(lib.tilePerms(tiles, needed, 4), (2*K(4-needed.length) - 2)/R(4));
     });
 
     it('should return the probability of getting mahjong', function() {
@@ -121,23 +138,28 @@ describe('lib', function() {
       var needed = parseRack('F 0');
       var R = nCr.bind(this, tiles.length);
       var K = nCr.bind(this, tiles.length - needed.length);
-      var duplicates = 2;
+      var duplicates = 3;
       for (var i = needed.length; i <= tiles.length; i++) {
-        console.error('i', i);
-        var num = W(tiles.length, needed.length, i, duplicates);
+        var num = W(tiles.length, needed.length, i, duplicates, 1);
         var den = R(i);
-        console.error('num', num);
-        console.error('den', den);
+        assert.equal(lib.tilePerms(tiles, needed, i), num/den);
+      }
+    });
+
+    it('should return the probability of getting mahjong', function() {
+      var tiles = parseRack('F 0 F GD RD F');
+      var needed = parseRack('F 0 F');
+      var R = nCr.bind(this, tiles.length);
+      var K = nCr.bind(this, tiles.length - needed.length);
+      var duplicates = 3;
+      for (var i = needed.length; i <= tiles.length; i++) {
+        var num = W(tiles.length, needed.length, i, duplicates, 2);
+        var den = R(i);
         assert.equal(lib.tilePerms(tiles, needed, i), num/den);
       }
       assert.equal(lib.tilePerms(tiles, needed, 0), 0);
       assert.equal(lib.tilePerms(tiles, needed, 1), 0);
-      assert.equal(lib.tilePerms(tiles, needed, 2), 3*K(2-needed.length)/R(2));
-      assert.equal(lib.tilePerms(tiles, needed, 3), (3*K(3-needed.length) - 3)/R(3));
-      assert.equal(lib.tilePerms(tiles, needed, 4), (3*K(4-needed.length) - (2+5+1))/R(4));
-      assert.equal(lib.tilePerms(tiles, needed, 4), (3*K(4-needed.length) - (3+3+2))/R(4));
-      //assert.equal(lib.tilePerms(tiles, needed, 5), (3*K(5-needed.length) - 4-3)/R(5));
-      //assert.equal(lib.tilePerms(tiles, needed, 5), P(tiles.length, needed.length, 5, 2));
+      assert.equal(lib.tilePerms(tiles, needed, 2), 0);
     });
 
     it('should be able to run on a whole tileset', function() {
